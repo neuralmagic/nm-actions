5c3fb9601614040088b4d2ed7a527a54c3cc7955
Tarun Kumar <takumar@redhat.com>
2025-03-18 20:45:59 +0530

    remove inconcnistencies
    Signed-off-by: Tarun Kumar <takumar@redhat.com>

--- actions/changed-files/check_changed_files.sh
+++ actions/changed-files/check_changed_files.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # THIS expects two environment variables to be present
 #  - INCLUDE_PATTERN
 #  - EXCLUDE_PATTERN
 
 set -ex
 
-if [[ -z ${GITHUB_BASE_REF} ]]; then
+if [ -z "${GITHUB_BASE_REF}" ]; then
   # this is not a pull request event, just get the previous commit
   previous_sha=$(git rev-parse --verify 'HEAD^{commit}')
 else
@@ -13,13 +13,13 @@ else
 fi
 CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRT ${previous_sha}..HEAD )
 
-if [ -z ${EXCLUDE_PATTERN} ]; then
+if [ -z "${EXCLUDE_PATTERN}" ]; then
   echo "${CHANGED_FILES}"
   exit 0
 fi
 
-IFS=',' read -r -a INCLUDE_PATTERNS <<< "${INCLUDE_PATTERN}"
-IFS=',' read -r -a EXCLUDE_PATTERNS <<< "${EXCLUDE_PATTERN}"
+IFS=$'\n' read -r -a INCLUDE_PATTERNS <<< "${INCLUDE_PATTERN}"
+IFS=$'\n' read -r -a EXCLUDE_PATTERNS <<< "${EXCLUDE_PATTERN}"
 MATCHED_FILES=""
 while IFS= read -r file; do
   # Check if the file matches any include pattern
@@ -39,8 +39,22 @@ while IFS= read -r file; do
       fi
       echo "Matched changed file: $file"
       MATCHED_FILES+="$file"$'\n'
+      if [ -n "${EXCLUDE_PATTERN}" ]; then
+        exclude_match=false
+        for exclude_pattern in "${EXCLUDE_PATTERNS[@]}"; do
+          if [[ "$file" =~ $exclude_pattern ]]; then
+            exclude_match=true
+            break
+          fi
+        done
+        if [ "$exclude_match" = true ]; then
+          break
+        fi
+      fi
+      echo "Matched changed file: $file"
+      MATCHED_FILES+="$file"$'\n'
       break
     fi
   done
 done <<< "$CHANGED_FILES"
-echo "all_changed_files=$MATCHED_FILES" >> $GITHUB_OUTPUT
+echo "all_changed_files=$MATCHED_FILES" >> "$GITHUB_OUTPUT"
