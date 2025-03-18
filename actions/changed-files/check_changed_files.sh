6b55131774031531d359de73fdef2cf35d6fd941
Tarun Kumar <takumar@redhat.com>
2025-03-18 20:45:59 +0530

    remove inconcnistencies
    Signed-off-by: Tarun Kumar <takumar@redhat.com>

--- actions/changed-files/check_changed_files.sh
+++ actions/changed-files/check_changed_files.sh
@@ -13,7 +13,7 @@ else
 fi
 CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRT ${previous_sha}..HEAD )
 
-if [[ -z ${INCLUDE_PATTERN} && -z ${EXCLUDE_PATTERN} ]]; then
+if [ -z ${EXCLUDE_PATTERN} ]; then
   echo "${CHANGED_FILES}"
   exit 0
 fi
@@ -25,19 +25,20 @@ while IFS= read -r file; do
   # Check if the file matches any include pattern
   for include_pattern in "${INCLUDE_PATTERNS[@]}"; do
     if [[ "$file" =~ $include_pattern ]]; then
-      # Check if the file matches any exclude pattern
-      exclude_match=false
-      for exclude_pattern in "${EXCLUDE_PATTERNS[@]}"; do
-        if [[ "$file" =~ $exclude_pattern ]]; then
-          exclude_match=true
+      if [ -n "${EXCLUDE_PATTERN}" ]; then
+        exclude_match=false
+        for exclude_pattern in "${EXCLUDE_PATTERNS[@]}"; do
+          if [[ "$file" =~ $exclude_pattern ]]; then
+            exclude_match=true
+            break
+          fi
+        done
+        if [ "$exclude_match" = true ]; then
           break
         fi
-      done
-      # If not excluded, consider it a match
-      if ! $exclude_match; then
-        echo "Matched changed file: $file"
-        MATCHED_FILES+="$file"$'\n'
       fi
+      echo "Matched changed file: $file"
+      MATCHED_FILES+="$file"$'\n'
       break
     fi
   done
