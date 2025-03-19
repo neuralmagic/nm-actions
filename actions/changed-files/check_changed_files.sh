e4c0c0c6f6303a6c638fca406165ebbe596a56da
Tarun Kumar <takumar@redhat.com>
2025-03-19 19:28:23 +0530

    Add replacement for jt-action github action  (#46)
    * Add replacement for jt action
    
    Signed-off-by: Tarun Kumar <takumar@redhat.com>
    
    * srent
    
    Signed-off-by: Tarun Kumar <takumar@redhat.com>
    
    * remove env
    
    Signed-off-by: Tarun Kumar <takumar@redhat.com>
    
    * adrress comment: add early exit code
    
    Signed-off-by: Tarun Kumar <takumar@redhat.com>
    
    * chnage the output name
    
    Signed-off-by: Tarun Kumar <takumar@redhat.com>
    
    * remove inconcnistencies
    
    Signed-off-by: Tarun Kumar <takumar@redhat.com>
    
    * remove inconcnistencies
    
    Signed-off-by: Tarun Kumar <takumar@redhat.com>
    
    * remove duplicate code
    
    Signed-off-by: Tarun Kumar <takumar@redhat.com>
    
    * new
    
    Signed-off-by: Tarun Kumar <takumar@redhat.com>
    
    ---------
    
    Signed-off-by: Tarun Kumar <takumar@redhat.com>

---
+++ actions/changed-files/check_changed_files.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# THIS expects two environment variables to be present
+#  - INCLUDE_PATTERN
+#  - EXCLUDE_PATTERN
+
+set -ex
+
+if [ -z "${GITHUB_BASE_REF}" ]; then
+	# this is not a pull request event, just get the previous commit
+	previous_sha=$(git rev-parse --verify 'HEAD^{commit}')
+else
+	previous_sha=${GITHUB_BASE_REF}
+fi
+CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRT ${previous_sha}..HEAD)
+
+IFS=$'\n' read -r -a INCLUDE_PATTERNS <<<"${INCLUDE_PATTERN}"
+IFS=$'\n' read -r -a EXCLUDE_PATTERNS <<<"${EXCLUDE_PATTERN}"
+MATCHED_FILES=""
+while IFS= read -r file; do
+	# Check if the file matches any include pattern
+	for include_pattern in "${INCLUDE_PATTERNS[@]}"; do
+		if [[ ! "$file" =~ $include_pattern ]]; then
+			continue
+		fi
+
+		if [[ ${EXCLUDE_PATTERNS[@]} -eq 0 ]]; then
+			MATCHED_FILES+="file"$'\n'
+			continue
+		fi
+
+		exclude_match=false
+		for exclude_pattern in "${EXCLUDE_PATTERNS[@]}"; do
+			if [[ "$file" =~ $exclude_pattern ]]; then
+				exclude_match=true
+				break
+			fi
+		done
+		if [ "$exclude_match" = true ]; then
+			break
+		fi
+		echo "Matched changed file: $file"
+		MATCHED_FILES+="$file"$'\n'
+	done
+done <<<"$CHANGED_FILES"
+
+echo "all_changed_files=$MATCHED_FILES" >>"$GITHUB_OUTPUT"
